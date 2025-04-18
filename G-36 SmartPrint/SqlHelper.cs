86fe418c228597efb67bc699b1a1628497c3712f
Areeb Ali <aareeb273@gmail.com>
2025-04-18 21:45:47 +0500

    Sqlhelper

---
+++ G-36 SmartPrint/SqlHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+using System.Data;
+using System.Data.SqlClient;
+namespace G_36_SmartPrint
+
+{
+    public class SqlHelper
+    {
+        public static string constring = "Server = localhost;Uid=root;Pwd=1234;Database= assignmentdb";
+        public static void executeDML(string dml)
+        {
+            MySqlConnection con = new MySqlConnection(constring);
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand(dml, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+
+        }
+
+        public static DataTable getDataTable(string sql)
+        {
+            MySqlConnection con = new MySqlConnection(constring);
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            return dt;
+        }
+    }
+}
